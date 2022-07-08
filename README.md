bfb3ea18ae59fe2021fe1a43c7cb9a9a1c02870c
SnehGhetiya <connectwithsneh@gmail.com>
2022-07-08 12:14:25 +0530

    Readme file added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fake Random Name Generator
+
+> A simple node app that generates random names for your websites
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const app = require("./src/index");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
